824827219cc17f7c7ad9a5fd22e4c776d00a9d35
María <151024318+meriwen@users.noreply.github.com>
2026-03-04 12:59:43 +0100

    Update letter.js

--- src/scenes/letter.js
+++ src/scenes/letter.js
@@ -37,7 +37,7 @@ export default class Letter extends Phaser.Scene {
 
     this.letterText = this.add.text(0, 0, "", {
       fontFamily: "VT323, monospace",
-      fontSize: "18px",
+      fontSize: "25px",
       color: "#4f342d",
       wordWrap: { width: this.textArea.width },
     
